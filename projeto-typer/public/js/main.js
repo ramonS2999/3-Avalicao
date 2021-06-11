8e2aed7d9c65df5e442ed8e7ae83b49c65b48ff7
taticalzero <taticalzero75@outlook.com>
2021-06-11 13:15:42 -0300

    Organizando o codigo e colocando o css

--- projeto-typer/public/js/main.js
+++ projeto-typer/public/js/main.js
@@ -1,43 +1,59 @@
-
-// Leitura da frase e puxar o seu tamanho
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
-// Campo de digitação
+// Campo e Tempo
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
+var tempoInicial = $("#tempo-digitacao").text();
+
+
+// triggers das funções -- Ativadores
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+   
+// Leitura dos campos
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+// Campo de digitação 
+function inicializaContadores() {
+    campo.on("input", function() {
     var conteudo = campo.val();
     var qtdPalavras = conteudo.split(/\S+/).length - 1;
     $("#contador-palavras").text(qtdPalavras);
-
-    var conteudo = campo.val(); 
-    var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
-    var qtdCaracteres = conteudoSemEspaco.length;
-    $('#contador-caracteres').text(qtdCaracteres);
-
-});
-
-// Contar o tempo Restante
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
- var cronometroID = setInterval(function() {
- tempoRestante--;
- $("#tempo-digitacao").text(tempoRestante);
- if (tempoRestante < 1) {
- campo.attr("disabled", true);
- clearInterval(cronometroID);
- }
- }, 1000);
-});
-
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+// Iniciar os Contadores
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+    var cronometroID = setInterval(function() {
+    tempoRestante--;
+    $("#tempo-digitacao").text(tempoRestante);
+    if (tempoRestante < 1) {
+    campo.attr("disabled", true);
+    clearInterval(cronometroID);
+    campo.toggleClass("campo-desativado");
+    }
+    }, 1000);
+    });
+}
 // Reiniciar o jogo
-$("#botao-reiniciar").click(function(){
-    campo.attr("disabled", false);
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
-    $("#tempo-digitacao").text(tempoInicial); //novo
-});
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+}
+
+
