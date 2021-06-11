2b229c84708e105badc00be92efb5d0eb84dafcd
taticalzero <taticalzero75@outlook.com>
2021-06-11 12:18:49 -0300

    Introduzindo o botão de reinciar e o timer

--- projeto-typer/public/js/main.js
+++ projeto-typer/public/js/main.js
@@ -1,12 +1,12 @@
 
-
+// Leitura da frase e puxar o seu tamanho
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
-
+// Campo de digitação
 var campo = $(".campo-digitacao");
 campo.on("input", function() {
     var conteudo = campo.val();
@@ -20,4 +20,24 @@ campo.on("input", function() {
 
 });
 
+// Contar o tempo Restante
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function() {
+ var cronometroID = setInterval(function() {
+ tempoRestante--;
+ $("#tempo-digitacao").text(tempoRestante);
+ if (tempoRestante < 1) {
+ campo.attr("disabled", true);
+ clearInterval(cronometroID);
+ }
+ }, 1000);
+});
 
+// Reiniciar o jogo
+$("#botao-reiniciar").click(function(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial); //novo
+});
